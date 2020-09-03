2450ed6b459bef4710511b403b293cd922a976cf
mauriciocerizza <mauriciocerizza@gmail.com>
2020-09-02 22:10:03 -0300

    Clase 01

---
+++ Ejemplo_Mascotas/Biblioteca/Mascota.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Mascota
+    {
+        private string nombre;
+        private DateTime fechaNacimiento;
+        private string especie;
+
+        public Mascota(string nombre, DateTime fechaNacimiento, string especie)
+        {
+            this.nombre = nombre;
+            this.fechaNacimiento = fechaNacimiento;
+            this.especie = especie;
+        } 
+
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public void SetNombre(string nuevoNombre)
+        {
+            this.nombre = nuevoNombre;
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // Con AppendFormat() y \n
+            stringBuilder.AppendFormat("Nombre: {0}\n", this.nombre.ToUpper());
+
+            // Con AppendLine() y Template Strings
+            stringBuilder.AppendLine($"Fecha de Nacimiento (custom): {this.fechaNacimiento.ToString("dd - MM - yyyy")}");
+            stringBuilder.AppendLine($"Fecha de Nacimiento (corta): {this.fechaNacimiento.ToShortDateString()}");
+            stringBuilder.AppendLine($"Fecha de Nacimiento (larga): {this.fechaNacimiento.ToLongDateString()}");
+            
+            // Con AppendLine() y string.Format()
+            stringBuilder.AppendLine(String.Format("Especie: {0}", this.especie));
+
+            return stringBuilder.ToString();
+        }
+
+        public int CalcularEdad()
+        {
+            DateTime fechaActual = DateTime.Today;
+
+            int edad = fechaActual.Year - this.fechaNacimiento.Year;
+
+            if (this.fechaNacimiento.AddYears(edad) > fechaActual)
+            {
+                edad--;
+            }
+
+            return edad;
+        }      
+    }
+}
---
+++ Ejemplo_Mascotas/Consola/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Biblioteca;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar las 4 mascotas
+            Mascota salem = new Mascota("Salem", new DateTime(1990, 1, 1), "Gato");
+            Mascota buddy = new Mascota("Buddy", new DateTime(1989, 2, 9), "Perro");
+            Mascota perry = new Mascota("Perry", new DateTime(2005, 6, 5), "Ornitorrinco");
+            Mascota fatiga = new Mascota("Fatiga", new DateTime(2005, 11, 11), "Perro");
+            
+
+
+            // Mostrar en consola utilizando el método MostrarDatos que devuelve un string, los datos de mascota.
+            Console.WriteLine(salem.MostrarDatos());
+            Console.WriteLine(buddy.MostrarDatos());
+            Console.WriteLine(perry.MostrarDatos());
+            Console.WriteLine(fatiga.MostrarDatos());
+
+            // Realizar un método que devuelva la edad de la mascota. Usando DateTime (Devuelve un int).
+            // Mostrar "{nombre} tiene {edad} años", usando un getter para el nombre y el método que calcula la edad.
+            Console.WriteLine($"{salem.GetNombre()} tiene {salem.CalcularEdad()} años.");
+            Console.WriteLine($"{buddy.GetNombre()} tiene {buddy.CalcularEdad()} años.");
+            Console.WriteLine($"{perry.GetNombre()} tiene {perry.CalcularEdad()} años.");
+            Console.WriteLine($"{fatiga.GetNombre()} tiene {fatiga.CalcularEdad()} años.");
+            
+            Console.ReadKey();
+            Console.Clear();
+            
+            // Cambiar el nombre con un setter y mostrar sus datos antes y despues.
+            Mascota gato = new Mascota("Simba", new DateTime(2015, 10, 8), "Gato");
+            Console.WriteLine(gato.MostrarDatos());
+            gato.SetNombre("Nala");
+            Console.WriteLine(gato.MostrarDatos());
+
+
+            Console.ReadKey();
+        }
+    }
+}
